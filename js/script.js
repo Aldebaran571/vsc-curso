2ba7e13ccd1f0c6429812fc9728e224c5b534f59
john <munra05@gmail.com>
2024-08-20 10:00:52 -0500

    instalando vbainas raras

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ numA =20
 numB =30
 res = numA+numB
 console.log(res)
+    isdksd 
+        idid 
+        sessionStorage
+            kidfu 
+                id debugger
+                    if f}
+
+
+</div>
